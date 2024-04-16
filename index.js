6789c182301f2cf8ed550bdb2cf54fc9c47249b2
Ahamed Fahmi <it21037306@my.sliit.lk>
2024-04-16 17:12:51 +0530

    [Test]: Add get method for Testing

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const productRouter = require('./Route/Product');
-const reviewRouter = require('./Route/Review');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const productRouter = require("./Route/Product");
+const reviewRouter = require("./Route/Review");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Deployed to AWS !");
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connection to MongoDB successful');
+    console.log("Connection to MongoDB successful");
     app.listen(port, () => {
       console.log(`Server is running on ${port}`);
     });
@@ -24,5 +28,5 @@ mongoose
   });
 
 /* Routes */
-app.use('/api/Product', productRouter);
-app.use('/api/Review', reviewRouter);
+app.use("/api/Product", productRouter);
+app.use("/api/Review", reviewRouter);
