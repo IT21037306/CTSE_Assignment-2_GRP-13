01082ca73000a21b5abd2bb91660f2d490be0c81
Ahamed Fahmi <it21037306@my.sliit.lk>
2024-04-16 18:26:39 +0530

    [Test]: Updated Server Port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const reviewRouter = require("./Route/Review");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
